b241150842a940bd4ebe1cb199c1da1f79db374b
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-08-28 09:58:06 -0500

    base url

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.API_BASE_URL_DOCKER;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL_DOCKER;
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
