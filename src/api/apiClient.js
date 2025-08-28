f640b94976fe93ff65fbaa6317247813c05dd1aa
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-08-28 09:46:04 -0500

    change vite

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'http://localhost:3000/api';
-const API_BASE_URL_DOCKER = 'http://localhost:1640/api';
-
 const apiClient = axios.create({
-  baseURL: API_BASE_URL_DOCKER,
+  baseURL: process.env.API_BASE_URL_DOCKER || 'http://localhost:1640/api',
   // baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
