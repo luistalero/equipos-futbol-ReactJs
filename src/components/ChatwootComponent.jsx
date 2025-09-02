d1ad1205bf61704f3944111a74c84bc1b7bb3ba8
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-09-02 16:31:27 -0500

    feat: message futbol integration

--- src/components/ChatwootComponent.jsx
+++ src/components/ChatwootComponent.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
 
-const ChatwootComponent = () => {
-  useEffect(() => {
-    const BASE_URL = "https://chatwoot-production-299c.up.railway.app";
+// const ChatwootComponent = () => {
+//   useEffect(() => {
+//     const BASE_URL = "https://chatwoot-production-299c.up.railway.app";
     
-    function loadChatwootSDK(d, t) {
-      const g = d.createElement(t);
-      const s = d.getElementsByTagName(t)[0];
+//     function loadChatwootSDK(d, t) {
+//       const g = d.createElement(t);
+//       const s = d.getElementsByTagName(t)[0];
       
-      g.src = BASE_URL + "/packs/js/sdk.js";
-      g.async = true;
-      s.parentNode.insertBefore(g, s);
+//       g.src = BASE_URL + "/packs/js/sdk.js";
+//       g.async = true;
+//       s.parentNode.insertBefore(g, s);
       
-      g.onload = function() {
-        window.chatwootSDK.run({
-          websiteToken: 'qQDrTj1ue7ZzvTVAHXGtfmaE',
-          baseUrl: BASE_URL
-        });
-      };
-    }
+//       g.onload = function() {
+//         window.chatwootSDK.run({
+//           websiteToken: 'qQDrTj1ue7ZzvTVAHXGtfmaE',
+//           baseUrl: BASE_URL
+//         });
+//       };
+//     }
     
-    loadChatwootSDK(document, "script");
+//     loadChatwootSDK(document, "script");
 
-    return () => {
-    };
-  }, []); 
+//     return () => {
+//     };
+//   }, []); 
   
-  return null; 
-};
+//   return null; 
+// };
 
-export default ChatwootComponent;
+// export default ChatwootComponent;
