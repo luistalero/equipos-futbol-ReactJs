381c88a05978b6bbd60482be541350c5a4e46754
Luis Alberto Talero Martinez <luis.talero@wiedii.co>
2025-09-02 10:30:32 -0500

    feat: new component chat

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,8 +1,13 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext, createContext } from 'react';
 import { jwtDecode } from 'jwt-decode';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-const useAuth = () => {
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(() => localStorage.getItem('token'));
   const [role, setRole] = useState(() => localStorage.getItem('role'));
   const [userId, setUserId] = useState(() => {
@@ -19,10 +24,9 @@ const useAuth = () => {
   });
 
   const [isSuspendedModalVisible, setIsSuspendedModalVisible] = useState(false);
-  const handleModalClose = () => {
-    setIsSuspendedModalVisible(false);
-  };
-  
+  const [isAuthErrorModalVisible, setIsAuthErrorModalVisible] = useState(false);
+  const navigate = useNavigate();
+
   const API_URL = import.meta.env.VITE_API_BASE_URL_DOCKER;
   const VITE_WS_URL = import.meta.env.VITE_WS_URL;
 
@@ -33,35 +37,30 @@ const useAuth = () => {
     setRole(null);
     setUserId(null);
     setIsSuspendedModalVisible(false);
+    setIsAuthErrorModalVisible(false);
+    navigate('/login');
   };
 
+  const handleModalClose = () => {
+    setIsSuspendedModalVisible(false);
+    setIsAuthErrorModalVisible(false);
+  };
+  
+  // Efecto para manejar la lógica de autenticación y la validación del token
   useEffect(() => {
-    const checkTokenExpiration = () => {
+    const checkTokenValidity = async () => {
       const storedToken = localStorage.getItem('token');
       if (storedToken) {
         try {
           const decodedToken = jwtDecode(storedToken);
           const currentTime = Date.now() / 1000;
           if (decodedToken.exp < currentTime) {
+            console.log('Token expirado.');
             logout();
-            alert('Tu sesión ha expirado. Por favor, inicia sesión de nuevo.');
+            setIsAuthErrorModalVisible(true);
             return false;
           }
-        } catch (error) {
-          logout();
-          return false;
-        }
-      }
-      return true;
-    };
-
-    const checkUserSuspension = async () => {
-      const storedToken = localStorage.getItem('token');
-      if (storedToken) {
-        try {
-          const decodedToken = jwtDecode(storedToken);
-          const currentUserId = decodedToken.id;
-          const response = await axios.get(`${API_URL}/users/${currentUserId}/status`, {
+          const response = await axios.get(`${API_URL}/users/${decodedToken.id}/status`, {
             headers: { Authorization: `Bearer ${storedToken}` }
           });
           if (response.data.is_suspended) {
@@ -69,42 +68,50 @@ const useAuth = () => {
             setIsSuspendedModalVisible(true);
           }
         } catch (error) {
+          console.error('Error al validar el token o el estado del usuario:', error);
           if (error.response && error.response.status === 403) {
             logout();
-            alert('Tu sesión ha sido invalidada. Por favor, inicia sesión de nuevo.');
+            setIsAuthErrorModalVisible(true);
           }
         }
       }
     };
+    checkTokenValidity();
+  }, [token, API_URL]);
 
-    checkTokenExpiration();
-    checkUserSuspension();
-
-    const ws = new WebSocket(VITE_WS_URL);
-    ws.onopen = () => {
-      if (token) {
-        ws.send(JSON.stringify({ type: 'auth', token }));
-      }
-    };
-    ws.onmessage = (event) => {
-      try {
-        const message = JSON.parse(event.data);
-        if (message.type === 'user-status' && message.status === 'suspended') {
-            logout();
-            setIsSuspendedModalVisible(true);
+  // Efecto para manejar la conexión de WebSocket
+  useEffect(() => {
+    let ws;
+    if (userId) { // Solo conecta si el userId está disponible
+      ws = new WebSocket(VITE_WS_URL);
+      ws.onopen = () => {
+        console.log('Conectado al WebSocket.');
+        if (token) {
+          ws.send(JSON.stringify({ type: 'auth', token }));
         }
-      } catch (error) {
-        console.error('Error al procesar el mensaje del WebSocket:', error);
+      };
+      ws.onmessage = (event) => {
+        try {
+          const message = JSON.parse(event.data);
+          if (message.type === 'user-status' && message.status === 'suspended') {
+              logout();
+              setIsSuspendedModalVisible(true);
+          }
+        } catch (error) {
+          console.error('Error al procesar el mensaje del WebSocket:', error);
+        }
+      };
+      ws.onclose = () => { console.log('Conexión WebSocket cerrada.'); };
+      ws.onerror = (error) => { console.error('Error en el WebSocket:', error); };
+    }
+    
+    // Función de limpieza para cerrar la conexión del WebSocket
+    return () => {
+      if (ws) {
+        ws.close();
       }
     };
-    ws.onclose = () => { };
-    ws.onerror = (error) => { };
-
-    return () => {
-      ws.close();
-    };
-    
-  }, [token, VITE_WS_URL, API_URL]);
+  }, [userId, token, VITE_WS_URL]);
 
   const login = (jwtToken, userRole) => {
     localStorage.setItem('token', jwtToken);
@@ -122,7 +129,17 @@ const useAuth = () => {
   const isAuthenticated = !!token;
   const isAdmin = role === 'admin';
 
-  return { token, isAuthenticated, isAdmin, userId, isSuspendedModalVisible, handleModalClose, login, logout };
+  return { 
+    token, 
+    isAuthenticated, 
+    isAdmin, 
+    userId, 
+    isSuspendedModalVisible, 
+    isAuthErrorModalVisible,
+    handleModalClose, 
+    login, 
+    logout 
+  };
 };
 
 export default useAuth;
